1e5fa4788cb7777382085bfb17c0494dd28c4ef7
mansib-uddin101 <drivemansib@gmail.com>
2026-03-09 23:11:45 +0600

    finished home display cards

--- script/home.js
+++ script/home.js
@@ -23,8 +23,12 @@ const priorityStyles = {
         badge: "bg-slate-100 text-[#64748B]"
     }
 };
-
-
+const labelStyles = {
+    bug: "bg-red-50 text-[#EF4444]",
+    "help wanted": "bg-amber-50 text-[#F59E0B]",
+    enhancement: "bg-emerald-50 text-[#10B981]",
+    default: "bg-slate-100 text-slate-600"
+};
 function renderIssues() {
     const cardContainer = document.getElementById("issuesContainer");
     cardContainer.innerHTML = '';
@@ -37,11 +41,11 @@ function renderIssues() {
         const style = priorityStyles[issue.priority] || priorityStyles.low;
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="bg-white border border-slate-200 p-4 rounded-lg flex flex-col gap-3">
+        <div class="bg-white h-full shadow-md border-t-4 p-4 rounded-lg flex flex-col gap-3 ${issue.status === 'open' ? 'border-green-500' : 'border-purple-700'}">
             <div class="flex items-center justify-between">
                 <img src="assets/${issue.status}-Status.png" alt="">
                 <span
-                    class="inline-block px-7 py-2 rounded-full bg-red-50 text-[#EF4444] font-medium text-sm tracking-wider
+                    class="inline-block px-7 py-2 rounded-full font-medium text-sm tracking-wider
                     ${style.badge}">
                     ${issue.priority.toUpperCase()}
                 </span>
@@ -51,11 +55,14 @@ function renderIssues() {
                 <p class="text-[#64748B]">${issue.description}</p>
             </div>
             <div class="border-b pb-4 border-slate-200 flex flex-wrap gap-2">
-                ${issue.labels.map(label => `
-                <span class="px-3 py-1 rounded-full bg-slate-100 text-slate-600 font-medium text-xs uppercase">
-                    ${label}
-                </span>
-            `).join('')}
+                ${issue.labels.map(label => {
+                const colorClass = labelStyles[label.toLowerCase()] || labelStyles.default;
+                return `
+                    <span class="inline-flex items-center gap-1 px-3 py-1 rounded-full font-bold text-sm uppercase border border-current opacity-80 ${colorClass}">
+                        ${label}
+                    </span>
+                `;
+            }).join('')}
             </div>
             <div class="flex flex-col gap-2 text-[#64748b] text-sm">
             <p>#${issue.id} by <span class="font-medium">${issue.author}</span></p>
